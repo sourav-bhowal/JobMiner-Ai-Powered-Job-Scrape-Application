13b7c00e8f6e673fecaea5c51bf62c23b804fc94
Sourav Bhowal <souravbhowal15@gmail.com>
2025-03-15 15:52:14 +0530

    frontend initailize

---
+++ apps/frontend/src/components/auth/AuthPage.tsx
@@ -0,0 +1,9 @@
+// Props type for AuthPage component
+interface AuthPageProps {
+  isSignIn: boolean;
+}
+
+// AuthPage component
+export default function AuthPage({ isSignIn }: AuthPageProps) {
+  return <div>AuthPage</div>;
+}
