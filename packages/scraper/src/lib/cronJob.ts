1e44260115500722d3da850452b3aaeade5a772a
Sourav Bhowal <souravbhowal15@gmail.com>
2025-03-19 22:52:08 +0530

    fixes

--- packages/scraper/src/lib/cronJob.ts
+++ packages/scraper/src/lib/cronJob.ts
@@ -3,15 +3,15 @@ import cron from "node-cron";
 
 // Clean up old scraped jobs every month
 export function startScrapedJobCleanup() {
-    // Run every month
-    cron.schedule("0 0 1 * *", async () => {
-      await prisma.job.deleteMany({
-        where: {
-          createdAt: {
-            lte: new Date(new Date().setMonth(new Date().getMonth() - 1)), // Delete jobs created more than a month ago
-          },
+  // Run every month
+  cron.schedule("0 0 1 * *", async () => {
+    await prisma.job.deleteMany({
+      where: {
+        createdAt: {
+          lte: new Date(new Date().setMonth(new Date().getMonth() - 1)), // Delete jobs created more than a month ago
         },
-      });
-      console.log("Cleaned up old scraped jobs");
+      },
     });
-  }
+    console.log("Cleaned up old scraped jobs");
+  });
+}
